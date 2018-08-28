dbca9543df2b0236de98d68e0fba5340a91b616d
Iva Ivanova <iva.ivanova@egt-bg.com>
2018-08-28 11:39:27 +0300

    Finished ex2, Exercices.txt updated

--- Working_with_arrays/ex2.cpp
+++ Working_with_arrays/ex2.cpp
@@ -1,17 +1,28 @@
 #include "ex2.h"
 
+
+const unsigned int Ex2::m_aProbabilities[NUMBER_OF_FREQUENCY]
+                                = {0,0,1,2,3,4,5,6,5,4,3,2,1};
+
 Ex2::Ex2()
 {
     /* initialize random seed: */
     srand(time(0));
     m_aCalculatedValues.fill(0);
     m_aCount.fill(0);
+    m_aIdealResultPercentage.fill(0);
+    m_aRealResultPercentage.fill(0);
+    m_aRealVsIdealProbabilities.fill(0);
     RollTheTwoDice();
 }
 
 Ex2::~Ex2()
 {
-
+    m_aCalculatedValues.fill(0);
+    m_aCount.fill(0);
+    m_aIdealResultPercentage.fill(0);
+    m_aRealResultPercentage.fill(0);
+    m_aRealVsIdealProbabilities.fill(0);
 }
 
 int Ex2::RollFirstDie()
@@ -53,7 +64,6 @@ void Ex2::RollTheTwoDice()
     CountTheSums();
 }
 
-
 void Ex2::CountTheSums()
 {
     for (unsigned i = 0; i < m_aCalculatedValues.size(); ++i)
@@ -63,38 +73,128 @@ void Ex2::CountTheSums()
         ++m_aCount[m_aCalculatedValues.at(i)];
     }
 
-    Print();
+    CheckTheTotals();
 }
 
 void Ex2::Print()
 {
-    std::cout <<std::endl << "Counters : " << std::endl;
-    for (unsigned int j = 0; j < NUMBER_OF_FREQUENCY ; j++)
+    // print the titles
+    std::cout <<std::endl << "Counter : "<<setw(set*set)
+              << "Frequency : " <<setw(set*set)
+              << "    Probabilities      :"
+              << "  Ideal % : " <<setw(set*set)
+              << "   Real % : " <<setw(set*set)
+              << " Compared : "
+              <<std::endl;
+
+    // print the first horizontal line
+    for (unsigned int j = 0; j < line ; j++)
     {
         std::cout << "-";
     }
-
     std::cout <<std::endl;
 
+    // print the line with values
     for (unsigned i = 2; i < m_aCount.size(); i++)
     {
-        std::cout << "|" << std::setw(set - 1)<< i << std::setw(set)<< "| - |" << std::setw(set)<< m_aCount.at(i) << "|" << std::endl;
-        for (unsigned int j = 0; j < NUMBER_OF_FREQUENCY ; j++)
-        {
-            std::cout<< "-";
-        }
-        std::cout << std::endl;
+        std::cout << "|" << std::setw(set*2)<< i <<  " | "
+                  << std::setw(set*set) << m_aCount.at(i)
+                  << " | " << PrintTheProbabilities(i)
+                  << std::setw(set*set) << m_aIdealResultPercentage.at(i)
+                  << " | " << std::setw(set*set) << m_aRealResultPercentage.at(i)
+                  << " | " << std::setw(set*set) << std::boolalpha
+                  << m_aRealVsIdealProbabilities[i] << " | "
+                  << std::endl;
     }
 
+    // print the last horizontal line
+    for (unsigned int j = 0; j < line ; j++)
+    {
+        std::cout << "-";
+    }
     std::cout <<std::endl;
 
+}
+
+string Ex2::PrintTheProbabilities(int i)
+{
+    switch (i) {
+    case 2:
+        return "1+1                    |" ;
+        break;
+    case 3:
+        return "2+1 1+2                |" ;
+        break;
+    case 4:
+        return "2+2 1+3 3+1            |" ;
+        break;
+    case 5:
+        return "3+2 2+3 4+1 1+4        |" ;
+        break;
+    case 6:
+        return "3+3 2+4 4+2 1+5 5+1    |" ;
+        break;
+    case 7:
+        return "4+3 3+4 5+2 2+5 1+6 6+1|" ;
+        break;
+    case 8:
+        return "4+4 3+5 5+3 2+6 6+2    |" ;
+        break;
+    case 9:
+        return "4+5 5+4 3+6 6+3        |" ;
+        break;
+    case 10:
+        return "5+5 4+6 6+4            |" ;
+        break;
+    case 11:
+        return "6+5 5+6                |" ;
+        break;
+    case 12:
+        return "6+6                    |" ;
+        break;
+    default:
+        break;
+    }
+
+    return "";
 }
 
 void Ex2::CheckTheTotals()
 {
+    // calculate the ideal percentage of probabilities
+    for (unsigned i = 2; i < NUMBER_OF_FREQUENCY; i++)
+    {
+#ifdef DEBUG
+        std::cout << "ideal : " << i << ((static_cast<double>(m_aProbabilities[i])/NUMBER_OF_ROLLED_TIMES)*100) <<std::endl;
+#endif
+        m_aIdealResultPercentage.at(i) = ((static_cast<double>(m_aProbabilities[i])/NUMBER_OF_ROLLED_TIMES)*100);
+    }
 
+    // calculate the real percentage of probabilities
+    for (unsigned i = 2; i < NUMBER_OF_FREQUENCY; i++)
+    {
+#ifdef DEBUG
+        std::cout << "real : " << i << ((static_cast<double>(m_aCount[i])/11)*100)<<std::endl;
+#endif
+        m_aRealResultPercentage.at(i) = ((static_cast<double>(m_aCount[i])/NUMBER_OF_ROLLED_TIMES)*100);
+    }
+
+    // compare ideal to real result
+    for (unsigned i = 2; i < NUMBER_OF_FREQUENCY; i++)
+    {
+        if (m_aRealResultPercentage.at(i) > m_aIdealResultPercentage.at(i)
+            || m_aRealResultPercentage.at(i) < m_aIdealResultPercentage.at(i))
+        {
+            m_aRealVsIdealProbabilities[i] = false;
+        }
+        else if (m_aRealResultPercentage.at(i) == m_aIdealResultPercentage.at(i))
+        {
+            m_aRealVsIdealProbabilities[i] = true;
+        }
+    }
+
+    Print();
 }
-
 
 void Ex2::PrintCalculatedValues()
 {
--- Working_with_arrays/ex2.h
+++ Working_with_arrays/ex2.h
@@ -18,20 +18,26 @@ using std::array;
 using std::rand;
 using std::srand;
 using std::setw;
+using std::string;
 
 class Ex2
 {
 private:
 
-    static const unsigned int NUMBER_OF_ROLLED_TIMES = 36000;
-    static const unsigned int NUMBER_OF_FREQUENCY    = 13;
+    static const unsigned int NUMBER_OF_ROLLED_TIMES  = 11;
+    static const unsigned int NUMBER_OF_FREQUENCY     = 13;
+
+    // array that shows how many ways there are
+    // to get a number
+    static const unsigned int m_aProbabilities[NUMBER_OF_FREQUENCY];
+
     // for setw
-    static const unsigned int set                   = 3;
+    static const unsigned int set               = 3;
     // DEBUG :: number which will be used to make the table
-    // must be divided by 10 (% = 0)
-    static const unsigned int number                 = 50;
+    // must be divided by 10 (% == 0)
+    static const unsigned int number            = 20;
     // DEBUG :: number of - for the line above below and between the numbers
-    static const unsigned int line                   = number + (set * number) +1;
+    static const unsigned int line              = number + (set * number) +1;
 
     // the two dice
     int m_iFirstDie;
@@ -43,17 +49,27 @@ private:
     // array which contains the frequency of the calculated values
     std::array<int, NUMBER_OF_FREQUENCY> m_aCount;
 
+    // result array with the ideal percentage of probabilities
+    std::array<int, NUMBER_OF_FREQUENCY> m_aIdealResultPercentage;
+    // result array with the real percentage of probabilities
+    std::array<int, NUMBER_OF_FREQUENCY> m_aRealResultPercentage;
+    // array that shows if real is like ideal probabilities
+    std::array<bool, NUMBER_OF_FREQUENCY> m_aRealVsIdealProbabilities;
+
     // roll the two different dice and return the result
     int RollFirstDie( );
     int RollSecondDie( );
 
     void PrintCalculatedValues();
+    std::string PrintTheProbabilities(int i);
+
 
 public:
     Ex2();
     virtual~Ex2();
 
-    std::array<int, NUMBER_OF_ROLLED_TIMES> GetTheCalculatedValues(){return m_aCalculatedValues;}
+    std::array<int, NUMBER_OF_ROLLED_TIMES> GetTheCalculatedValues()
+                                        {return m_aCalculatedValues;}
 
     // roll the dice 36 000 times and save the calculated values
     void RollTheTwoDice();
