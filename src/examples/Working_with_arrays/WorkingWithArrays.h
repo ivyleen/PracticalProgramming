feeba1f14df25de2edd38352e6646ad3ce81af8c
Ivy Ivanova <ivaivanova091@gmail.com>
2019-10-13 13:12:26 +0100

    adding the new files into the build task

--- src/examples/Working_with_arrays/WorkingWithArrays.h
+++ src/examples/Working_with_arrays/WorkingWithArrays.h
@@ -8,34 +8,37 @@
     switch (number)
     {
     case 1:
-        // Ex1
-        /*  
-        std::cout << std::endl << "Ex1 begins here." << std::endl;
-        std::cout << "Fist try" << std::endl;
-        vector<int> gross = {1234,4363,1493, 654, 24523};
-        Ex1 paytable;
-        paytable.SetBaseCommission(200);
-        paytable.SetPercentOfGross(9);
-        paytable.SetGrosses(gross);
-        std::cout << "We set the correct values." << std::endl;
-        paytable.CountHowManyPeople();
-        std::cout << std::endl << "Second try" << std::endl;
-        Ex1 paytable2(300, 10, gross);
-        std::cout << "Ex1 ends here."<< std::endl;
-        */
+        {
+            std::cout << std::endl << "Ex1 begins here." << std::endl;
+            std::cout << "Fist try" << std::endl;
+            vector<int> gross = {1234,4363,1493, 654, 24523};
+            Ex1 paytable;
+            paytable.SetBaseCommission(200);
+            paytable.SetPercentOfGross(9);
+            paytable.SetGrosses(gross);
+            std::cout << "We set the correct values." << std::endl;
+            paytable.CountHowManyPeople();
+            std::cout << std::endl << "Second try" << std::endl;
+            Ex1 paytable2(300, 10, gross);
+            std::cout << "Ex1 ends here."<< std::endl;
+        }
         break;
     case 2:
-        // Ex2
-        /*std::cout << std::endl << "Ex2 begins here." << std::endl;
-        Ex2 SecondEx;
-        std::cout << "Ex2 ends here."<< std::endl;*/
+        {
+            std::cout << std::endl << "Ex2 begins here." << std::endl;
+            Ex2 SecondEx;
+            std::cout << "Ex2 ends here."<< std::endl;
+        }
         break;
     case 3:
-        //std::cout << std::endl << "Ex3 begins here." << std::endl;
-        //std::cout << "Ex3 ends here."<< std::endl;
-     
+        {
+            std::cout << std::endl << "Ex3 begins here." << std::endl;
+            std::cout << "Ex3 ends here."<< std::endl;
+        }
+        break;
      default:
-         break;
+        cout << "There isn't such example." << endl;
+        break;
      }
 
  }
