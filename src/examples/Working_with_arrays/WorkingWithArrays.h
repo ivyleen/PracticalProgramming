bafb71fba34e319d8e8b139fad2674006b904465
Ivy Ivanova <ivaivanova091@gmail.com>
2019-10-13 12:25:07 +0100

    Making a menu for the new examples

---
+++ src/examples/Working_with_arrays/WorkingWithArrays.h
@@ -0,0 +1,41 @@
+#include "ex1.h"
+#include "ex2.h"
+#include "Ex3/ex3.h"
+ 
+ void ChooseExampleWithArrays(int number)
+ {
+
+    switch (number)
+    {
+    case 1:
+        // Ex1
+        /*  
+        std::cout << std::endl << "Ex1 begins here." << std::endl;
+        std::cout << "Fist try" << std::endl;
+        vector<int> gross = {1234,4363,1493, 654, 24523};
+        Ex1 paytable;
+        paytable.SetBaseCommission(200);
+        paytable.SetPercentOfGross(9);
+        paytable.SetGrosses(gross);
+        std::cout << "We set the correct values." << std::endl;
+        paytable.CountHowManyPeople();
+        std::cout << std::endl << "Second try" << std::endl;
+        Ex1 paytable2(300, 10, gross);
+        std::cout << "Ex1 ends here."<< std::endl;
+        */
+        break;
+    case 2:
+        // Ex2
+        /*std::cout << std::endl << "Ex2 begins here." << std::endl;
+        Ex2 SecondEx;
+        std::cout << "Ex2 ends here."<< std::endl;*/
+        break;
+    case 3:
+        //std::cout << std::endl << "Ex3 begins here." << std::endl;
+        //std::cout << "Ex3 ends here."<< std::endl;
+     
+     default:
+         break;
+     }
+
+ }
--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,7 @@
 #include "examples/Basics.h"
 #include "examples/ControlFlowStatementsExamples.h"
 #include "examples/RecursiveExamples.h"
-#include "examples/Working_with_arrays/ex1.h"
-#include "examples/Working_with_arrays/ex2.h"
-#include "examples/Working_with_arrays/Ex3/ex3.h"
+#include "examples/Working_with_arrays/WorkingWithArrays.h"
 
 int main ()
 {
@@ -61,32 +59,8 @@ int main ()
     //BinaryToOctal();
     //OctalToBinary();
 
-    //working with arrays
-        // Ex1
-/*    std::cout << std::endl << "Ex1 begins here." << std::endl;
-    std::cout << "Fist try" << std::endl;
-
-    vector<int> gross = {1234,4363,1493, 654, 24523};
-
-    Ex1 paytable;
-
-    paytable.SetBaseCommission(200);
-    paytable.SetPercentOfGross(9);
-    paytable.SetGrosses(gross);
-    std::cout << "We set the correct values." << std::endl;
-    paytable.CountHowManyPeople();
-
-    std::cout << std::endl << "Second try" << std::endl;
-    Ex1 paytable2(300, 10, gross);
-    std::cout << "Ex1 ends here."<< std::endl;
-*/
-    // Ex2
-    /*std::cout << std::endl << "Ex2 begins here." << std::endl;
-    Ex2 SecondEx;
-    std::cout << "Ex2 ends here."<< std::endl;*/
-
-    //std::cout << std::endl << "Ex3 begins here." << std::endl;
-    //std::cout << "Ex3 ends here."<< std::endl;
+    // working with arrays
+    ChooseExampleWithArrays(1);
     
     return 0;
 };
